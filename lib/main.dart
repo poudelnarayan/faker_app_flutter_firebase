9dfcc91d9caf64813e07e263f199519007087df9
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-07-08 21:47:49 +0545

    Stater Project

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+    return MaterialApp.router(
+      routerConfig: goRouter,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.indigo, // background (button) color
+            foregroundColor: Colors.white, // foreground (text) color
+          ),
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.indigo, // background (button) color
+          foregroundColor: Colors.white, // foreground (text) color
+        ),
+      ),
+    );
+  }
+}
