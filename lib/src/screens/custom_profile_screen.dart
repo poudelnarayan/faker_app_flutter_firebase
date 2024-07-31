173c0e1f9ef0433efa0b85878e34f8d79fd70782
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-07-31 17:06:03 +0545

    using authStateChanges() and the  refreshListenable argument

--- lib/src/screens/custom_profile_screen.dart
+++ lib/src/screens/custom_profile_screen.dart
@@ -1,8 +1,6 @@
-import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class CustomProfileScreen extends ConsumerWidget {
   const CustomProfileScreen({super.key});
@@ -13,11 +11,6 @@ class CustomProfileScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Profile'),
       ),
-      actions: [
-        SignedOutAction((context) {
-          context.goNamed(AppRoute.signIn.name);
-        })
-      ],
     );
   }
 }
