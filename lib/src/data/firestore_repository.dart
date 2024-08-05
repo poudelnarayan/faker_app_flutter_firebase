c3ba5f07f905ab0ae9805dfe7173dc6181226dca
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-08-05 22:50:13 +0545

    Adding update and delete operaions

--- lib/src/data/firestore_repository.dart
+++ lib/src/data/firestore_repository.dart
@@ -7,15 +7,31 @@ class FirestoreRepository {
   FirestoreRepository(this._firestore);
   final FirebaseFirestore _firestore;
 
-  Future<void> addJob(String uid, String title, String company) async {
-    final docRef = await _firestore.collection('jobs').add({
-      'uid': uid,
-      'title': title,
-      'company': company,
-    });
+  Future<void> addJob(String uid, String title, String company) =>
+      _firestore.collection('jobs').add(
+        {
+          'uid': uid,
+          'title': title,
+          'company': company,
+        },
+      );
 
-    debugPrint(docRef.id);
-  }
+  Future<void> updateJob(
+    String uid,
+    String jobId,
+    String title,
+    String company,
+  ) =>
+      _firestore.doc('jobs/$jobId').update(
+        {
+          'uid': uid,
+          'title': title,
+          'company': company,
+        },
+      );
+
+  Future<void> deleteJob(String uid, String jobId) =>
+      _firestore.doc('jobs/$jobId').delete();
 
   Query<Job> jobsQuery() {
     return _firestore.collection('jobs').withConverter(
